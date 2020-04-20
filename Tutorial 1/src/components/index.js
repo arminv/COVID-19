a38b7321ee21df8965aa5a7cd7be6039a920f796
arminv <arminv@users.noreply.github.com>
2020-04-20 00:47:03 -0400

    first commit

---
+++ Tutorial 1/src/components/index.js
@@ -0,0 +1,5 @@
+// This allows us to import all the components using {} in the App.js file:
+
+export { default as Cards } from "./Cards/Cards";
+export { default as Chart } from "./Chart/Chart";
+export { default as CountryPicker } from "./CountryPicker/CountryPicker";
