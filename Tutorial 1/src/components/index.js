f62674620a238e53285af6d25a0ace7b4e448553
arminv <arminv@users.noreply.github.com>
2020-05-08 02:16:21 -0400

    Removed files

--- Tutorial 1/src/components/index.js
+++
@@ -1,5 +0,0 @@
-// This allows us to import all the components using {} in the App.js file:
-
-export { default as Cards } from "./Cards/Cards";
-export { default as Chart } from "./Chart/Chart";
-export { default as CountryPicker } from "./CountryPicker/CountryPicker";
